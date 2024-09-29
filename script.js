bf7609be0b677d1071e427e7d14ccddc5e53e24a
Renold Thomas <renoldthomasp@gmail.com>
2024-09-29 15:10:16 -0400

    Edit files to center and take inputs for grid size

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 const container = document.querySelector("#container");
+container.style.width = "500px";
 
-const GRID_SIZE = 16;
+let GRID_SIZE = 16;
 
-//To fill container with # of columns
-for (let i = 1; i <= GRID_SIZE; i++) {
-    const column = document.createElement("div");
-    column.classList.add("rows");
-
-    //To fill column with # of divs
-    for (let i = 1; i <= GRID_SIZE; i++) {
-        const div = document.createElement("div");
-        
-        //Add hover over effect for each div
-        div.addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor = "red";
-        })
-        column.appendChild(div);
-    }
-    container.appendChild(column);
+function changeSize() {
+    GRID_SIZE = prompt("Please enter desired grid size? (max 100)", 16);
+    makeGrid(GRID_SIZE);
 }
 
+function makeGrid(size) {
+    container.innerHTML = "";
+    
+    let cellSize = 500/size;
+
+
+    //To fill container with # of columns
+    for (let i = 1; i <= size; i++) {
+        const column = document.createElement("div");
+        column.classList.add("columns");
+
+        //To fill column with # of divs
+        for (let j = 1; j <= size; j++) {
+            const div = document.createElement("div");
+            
+            div.style.width = `${cellSize}px`;
+            div.style.height = `${cellSize}px`;
+
+            //Add hover over effect for each div
+            div.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = "red";
+            })
+            column.appendChild(div);
+        }
+        container.appendChild(column);
+    }
+}
+
+makeGrid(GRID_SIZE);
